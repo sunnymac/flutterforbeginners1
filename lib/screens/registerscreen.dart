3fb2f2f4944853c36e821deedda8def0198481ff
Sanjay Makwana <63788069+sunnymac@users.noreply.github.com>
2022-06-14 11:26:39 +0530

    init

---
+++ lib/screens/registerscreen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({Key? key}) : super(key: key);
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      body: SafeArea(child: Text("asdadadada")),
+    );
+    
+  }
+}
